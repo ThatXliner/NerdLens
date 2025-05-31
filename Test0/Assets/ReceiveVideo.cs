11a82b5b064647bd3a0b5bb6a821826d0996656e
ThatXliner <bryan.hu.2020@gmail.com>
2025-05-31 10:54:29 -0700

    I'm so bad at spelling

---
+++ Test0/Assets/ReceiveVideo.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.WebRTC;
+public class RecieveVIdeo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Get a valid RendertextureFormat
+        var gfxType = SystemInfo.graphicsDeviceType;
+        var format = WebRTC.GetSupportedRenderTextureFormat(gfxType);
+
+        // // Create a track from the RenderTexture
+        // var rt = new RenderTexture(width, height, 0, format);
+        // var track = new VideoStreamTrack("video", renderTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
